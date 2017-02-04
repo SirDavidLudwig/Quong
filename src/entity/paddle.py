bf879892d9a30f08d2c0f66704ab15bd7aea80df
Christopher Clouse <chc2t@mtmail.mtsu.edu>
2017-02-04 00:21:39 -0600

    Render paddle.

--- src/entity/paddle.py
+++ src/entity/paddle.py
@@ -1,4 +1,5 @@
 from . entity import *
+import os
 import pygame
 
 
@@ -6,11 +7,17 @@ class Paddle(Entity):
 
 	def __init__(self):
 
-		pass
+		base_path = os.path.dirname(os.path.realpath(__file__)) + "/"
+		self.__texture = pygame.image.load(base_path + "../../res/textures/paddle.png")
+		self.__x = 0
+		self.__y = 0
+		self.__rect = self.__texture.get_rect()
 
 
 	# @param
 	# dt: Delta Time
 	def draw(self, screen, dt):
 
-		pygame.draw.rect(screen.getSurface(), (0, 128, 255), pygame.Rect(0, 0, screen.getWidth(), screen.getHeight()))
+		self.__rect.left = self.__x
+		self.__rect.top = self.__y
+		screen.getSurface().blit(self.__texture, self.__rect)
